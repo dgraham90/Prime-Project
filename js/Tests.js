89595736625651abf1637a8631a0c5bdbc6a370f
Donnie Graham <donniegraham1@gmail.com>
2018-04-01 19:33:59 +0100

    Built a basic UI with an input field and a table element
    Began writing unit tests using the Jasmine framework

---
+++ js/Tests.js
@@ -0,0 +1,23 @@
+desc("Unit Tests for prime generation", function() { 
+    it("Array contains prime numbers", function() {
+
+    });
+    it("Array length should be equal to input", function() {
+
+    });
+});
+
+desc("Unit Tests for DOM", function() {
+    it("User input should be a whole number", function() {
+
+    });
+    it("User input should be >= to 1", function() {
+
+    });
+    it("Table generated n+1 wide and n+1 high", function() {
+
+    });
+    it("Generates table in less than 5 seconds", function() {
+
+    });
+});
