d8d0f235faf2d327376b39c23d8baf14065ce39f
Donnie Graham <donniegraham1@gmail.com>
2018-04-02 20:49:13 +0100

    Added Jasmin lib for unit testing
    Added some unit tests for prime number generation in js\Tests.js

--- js/Tests.js
+++ js/Tests.js
@@ -1,13 +1,34 @@
-desc("Unit Tests for prime generation", function() { 
-    it("Array contains prime numbers", function() {
+describe("Unit Tests for prime number generation", function() { 
+    let prime;
+    //Calculate a random number between 2 and 100
+    const arrayLength = Math.floor(Math.random() * 100) + 2;
 
+    beforeEach(function() {
+        //Add a matcher to test if a number is prime
+        jasmine.addMatchers({
+            isPrime: function(num) {
+                        for(let i = 2, s = Math.sqrt(num); i <= s; i++)
+                            if(num % i === 0) return false; 
+                        return num !== 1;
+                    }
+        });
+            
+        //before each test instantiate a new prime array
+        prime = new primes(arrayLength);
+    });
+
+    it("Prime Array isn't empty", function() {
+        expect(prime.primeArray.toBeGreaterThan(1));
     });
     it("Array length should be equal to input", function() {
-
+        expect(prime.primeArray.length.toBe(arrayLength));
+    });
+    it("Random number in Array is a prime number", function() {
+        expect(prime.primeArray[Math.floor(Math.random() * arrayLength)].isPrime());
     });
 });
 
-desc("Unit Tests for DOM", function() {
+describe("Unit Tests for DOM", function() {
     it("User input should be a whole number", function() {
 
     });
